0c6c6cfb5c9ba40c1e8d211941f81702556e1a23
Ben Lenington <ben2lening@gmail.com>
2024-05-01 16:07:42 -0500

    Update app text again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This app is hosted on AWS!
+          This app is hosted on AWS! (V2)
         </p>
         <a
           className="App-link"
